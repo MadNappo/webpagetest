5755bd4e10b1fee51ec7dc539b0cefa11673ea80
MadNappo <68039017+MadNappo@users.noreply.github.com>
2020-07-13 10:50:46 +0900

    Update ar_test.js

--- js/ar_test.js
+++ js/ar_test.js
@@ -157,8 +157,8 @@ function init() {
         cam_ctrl.dampingFactor = 0.2;
         cam_ctrl.rotateSpeed =0.5;
         cam_ctrl.enablePan = false;
-        cam_ctrl.minZoom = 0.5;
-        cam_ctrl.maxZoom = 3.0;
+        cam_ctrl.minZoom = 0.9;
+        cam_ctrl.maxZoom = 1.2;
         cam_ctrl.target.set(0, 0, 0);
     }
     scene.add(camera);
